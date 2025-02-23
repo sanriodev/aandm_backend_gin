8abfb35b9b5badef2d111e9146062cc9bc3a666a
Matteo Juen <matteo.juen@ematric.com>
2025-02-23 18:42:35 +0100

    feat: mongo host :)

--- internal/mongo/mongo.go
+++ internal/mongo/mongo.go
@@ -17,14 +17,11 @@ import (
 var client *mongo.Client
 
 func BootstrapDatabase() {
-
-	client, err := mongo.NewClient(options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%s", config.Config.MongoUser, config.Config.MongoPassword, config.Config.MongoHost, config.Config.MongoPort)))
-	if err != nil {
-		log.Fatal(err)
-	}
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
-	err = client.Connect(ctx)
+
+	var err error
+	client, err = mongo.Connect(ctx, options.Client().ApplyURI(fmt.Sprintf("mongodb://%s:%s@%s:%s", config.Config.MongoUser, config.Config.MongoPassword, config.Config.MongoHost, config.Config.MongoPort)))
 	if err != nil {
 		log.Fatal(err)
 	}
